8321e3ee8b8238481b53e86fb388a0e5f67d88fd
Andreas Rasmussen <andreasrefras@gmail.com>
2017-10-10 12:04:46 +0200

    Added custom scrollbar (and made it substitute right gold border) when on WebKit browsers

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="appContainer">
-    <div class="appBorders"><div/><div/><div/><div/></div>
+    <div class="appBorders">
+      <div class="top" />
+      <div class="right" v-if="!isWebkit" /> <!-- TODO: Also show if no scrollbars on page -->
+      <div class="bottom" />
+      <div class="left" />
+    </div>
     <app-header />
-    <router-view />
+    <router-view class="route" />
     <app-footer />
   </div>
 </template>
@@ -15,22 +20,45 @@
     components: {
       'app-header': Header,
       'app-footer': Footer
+    },
+    computed: {
+      isWebkit() {
+        const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+        const isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+        if ( isChrome || isSafari ) return true
+      }
     }
   }
 </script>
 
 <style lang="scss">
   @import '~@/styles/main';
-  .appContainer { width: 100%; min-height: 100%; }
-  .appBorders { // Solution made to take into account that everything underneath should be clickable
-    & * { position: fixed; background-color: $color-gold; }
-    & div:nth-child(1),
-    & div:nth-child(3) { height: 16px; width: 100%; }
-    & div:nth-child(2),
-    & div:nth-child(4) { width: 15px; height: 100%; } // Optically adjusted
-    & div:nth-child(1) { top: 0; left: 0; }
-    & div:nth-child(2) { top: 0; right: 0; }
-    & div:nth-child(3) { bottom: 0; left: 0; }
-    & div:nth-child(4) { top: 0; left: 0; }
+
+  .appContainer {
+    width: 100%;
+    min-height: 100%;
   }
+
+  .appBorders { // Solution made to take into account that everything underneath should be clickable
+    position: relative;
+    z-index: 1;
+    & * { position: fixed; background-color: $color-gold; }
+    & .top,
+    & .bottom { height: 16px; width: 100%; }
+    & .right,
+    & .left { width: 15px; height: 100%; } // Optically adjusted
+    & .top { top: 0; left: 0; }
+    & .right { top: 0; right: 0; }
+    & .bottom { bottom: 0; left: 0; }
+    & .left { top: 0; left: 0; }
+  }
+
+  .route {
+    width: 100%;
+    min-height: 100vh;
+    margin-top: -180px;
+    padding-top: 180px + $scale-8-1;
+    padding-bottom: 180px;
+  }
+
 </style>
