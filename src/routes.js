8178d740b966fc3ff8da9062958804038ccae707
Andreas Rasmussen <andreasrefras@gmail.com>
2017-10-20 00:24:54 +0200

    Simplified the new routing logic so that Welcome-page is back to using '/' so that it's the base route like 'Start' was + just appending automatically created WP routes to the main routes array instead of as children to a 'Page' route

--- src/routes.js
+++ src/routes.js
@@ -9,16 +9,13 @@ import merch from '@/routes/merch'
 import about from '@/routes/about'
 import contact from '@/routes/contact'
 
-import page from '@/routes/page' // Generic route -> Use as base for routes like /post/:id
-
 // Subsite components -> used below for mapping WP routes
 import Post from '@/components/Post'
 import Release from '@/components/Release'
 import Track from '@/components/Track'
 
 let routes = [
-  { path: '/', component: page, name: 'page', children: []},
-  { path: '/velkommen', component: welcome, name: 'velkommen' },
+  { path: '/', component: welcome, name: 'velkommen' },
   { path: '/nyheder', component: news, name: 'nyheder' },
   { path: '/koncerter', component: concerts, name: 'koncerter' },
   { path: '/musik', component: music, name: 'musik' },
@@ -28,7 +25,7 @@ let routes = [
 ]
 
 // Mapping WP routes to corresponding Vue route (wp.routes made available in functions.php)
-// So that everytime a post type is created in wp-admin a corresponding Vue route is created as childroute to the generic page route
+// So that everytime a post type is created in wp-admin a corresponding Vue route is created
 // Useful if substuff like release or tracks should have their own urls some time in the future
 const subsiteComponents = {
   post: Post,
@@ -40,19 +37,16 @@ for (let key in wp.routes) {
   if ( wp.routes[key].type != 'page' ) {
 
     const wpRoute = wp.routes[key]
+
     const vueRoute = {
-      path: '/' + wpRoute.type + '/:' + wpRoute.type + 'Id',
+      path: '/' + wpRoute.type + '/:id',
       component: subsiteComponents[wpRoute.type],
       name: wpRoute.type
     }
 
-    let genericRoute;
-    for ( let route of routes ) { if ( route.name === 'page' ) genericRoute = route }
     let routeAlreadyExists = false
-    for ( let i = 0; i < genericRoute.children.length; i++ ) {
-      if ( genericRoute.children[i].name === wpRoute.type ) routeAlreadyExists = true
-    }
-    if ( !routeAlreadyExists ) genericRoute.children.push(vueRoute)
+    for ( let route of routes ) { if ( route.name === wpRoute.type ) routeAlreadyExists = true }
+    if ( !routeAlreadyExists ) routes.push(vueRoute)
 
   }
 }
