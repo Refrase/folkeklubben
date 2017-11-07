d4e5255592406adcc795504777a773d14b34f86f
Andreas Rasmussen <andreasrefras@gmail.com>
2017-11-07 15:06:06 +0100

    Added favicon

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/dist/assets/icons/favicon.ico">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/dist/assets/icons/favicon-16x16.png" sizes="16x16" />
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/dist/assets/icons/favicon-32x32.png" sizes="32x32" />
   <style type="text/css">
     /* MailChimp error message */
     #mce-error-response {
