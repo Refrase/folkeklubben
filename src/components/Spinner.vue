2c029be9099655b7003e2eac62f56b12e744de45
Andreas Rasmussen <andreasrefras@gmail.com>
2017-11-07 17:27:58 +0100

    Enhanced spinner to spin the wheels

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,5 +1,9 @@
 <template>
-  <img src="../assets/images/folkeklubben-bike-darkblue.svg" width="200" class="loadingSpinner" alt="Loading..." />
+  <div class="bike">
+    <img src="../assets/images/stel.png" width="200" class="frame" />
+    <img src="../assets/images/baghjul.png" class="wheelBack" />
+    <img src="../assets/images/forhjul.png" width="200" class="wheelFront" />
+  </div>
 </template>
 
 <script>
@@ -8,16 +12,38 @@
 
 <style lang="scss" scoped>
   @import '~@/styles/vars';
+  @import '~@/styles/animations';
   @keyframes bikePassBy {
-    from { transform: rotate3d(0,1,0,180deg) translateX(calc(50vw - 200px)); }
-    to   { transform: rotate3d(0,1,0,180deg) translateX(calc(-100vw + 200px)); }
+    from { transform: rotate3d(0,1,0,-180deg) translateX(200px); }
+    to   { transform: rotate3d(0,1,0,-180deg) translateX(calc(-100vw + 200px)); }
   }
-  .loadingSpinner {
+  .bike {
     display: block;
     position: fixed;
+    width: 200px;
     bottom: 5px;
+    left: 0;
     opacity: 0.3;
-    transform: rotate3d(0,1,0,360deg);
     animation: bikePassBy 3s linear infinite;
+  }
+  .frame {
+    position: absolute;
+    bottom: 0;
+  }
+  .wheelFront {
+    position: absolute;
+    bottom: 9px;
+    left: 8px;
+    width: 55px;
+    height: 55px;
+    animation: rotate 0.2s linear infinite reverse;
+  }
+  .wheelBack {
+    position: absolute;
+    bottom: 9px;
+    right: 0px;
+    width: 68px;
+    height: 68px;
+    animation: rotate 0.2s linear infinite reverse;
   }
 </style>
