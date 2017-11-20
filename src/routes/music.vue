b076fbd9f8b78d1bfe7ea8e325cd5804a5e6d0b6
Andreas Rasmussen <andreasrefras@gmail.com>
2017-11-20 16:48:13 +0100

    Showing 2 first musicvideos as embed - the rest as buttons

--- src/routes/music.vue
+++ src/routes/music.vue
@@ -10,10 +10,13 @@
       </grid-block>
       <grid-block>
         <h1 class="span-12 margin-bottom-4-1">Musikvideoer</h1>
-        <div v-for="(video, index) in videosByReleasedateReversed" class="span-6">
-          <div class="videoWrap margin-bottom-4-1">
+        <div v-for="(video, index) in videosByReleasedateReversed" v-if="index < 2" class="span-6">
+          <div  class="videoWrap margin-bottom-4-1">
             <iframe class="video" :src="video.acf.video_embed_url_youtube" frameborder="0" gesture="media" allowfullscreen></iframe>
           </div>
+        </div>
+        <div v-else class="span-12 margin-bottom-2-1">
+          <a target="_blank" class="button" :href="video.acf.video_embed_url_youtube">{{ video.title.rendered }}</a>
         </div>
       </grid-block>
     </div>
