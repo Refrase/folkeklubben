14759f948345d05b59c19b7a7d5f8ee791218959
Andreas Rasmussen <andreasrefras@gmail.com>
2018-10-01 18:01:16 +0200

    Fixed that tours wasn't shown as the function comparing tour dates with today didn't account for .getMonth() returning months from 0

--- src/utils/today.js
+++ src/utils/today.js
@@ -2,7 +2,7 @@ export const today8Digits = {
   methods: {
     today8Digits() {
       const today = new Date()
-      const todayFull = today.getFullYear() + '' + ( today.getMonth() < 10 ? '0' : '' ) + ( today.getMonth() + 1 ) + '' + ( today.getDate() < 10 ? '0' : '' ) + today.getDate()
+      const todayFull = today.getFullYear() + '' + ( today.getMonth() < 9 ? '0' : '' ) + ( today.getMonth() + 1 ) + '' + ( today.getDate() < 10 ? '0' : '' ) + today.getDate()
       return parseInt(todayFull)
     }
   }
